1f3ba1a6c6f4b28664b423a729ae7c469d7fa2da
Saran440 <saranl@ecosoft.co.th>
2024-01-18 15:08:34 +0700

    [ENH] account_manual_currency: Enable manual currency entry during payment registration.
    - This feature allows users to utilize currency rate differences from the invoice.

--- account_manual_currency/models/account_move.py
+++ account_manual_currency/models/account_move.py
@@ -4,7 +4,8 @@
 from lxml import etree
 
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_is_zero
 
 
 class AccountMove(models.Model):
@@ -136,6 +137,28 @@ class AccountMove(models.Model):
             result["arch"] = etree.tostring(doc, encoding="unicode")
         return result
 
+    @api.onchange("manual_currency_rate")
+    def _onchange_manual_currency_rate(self):
+        last_rate = self.env["res.currency.rate"]._get_last_rates_for_companies(
+            self.company_id | self.env.company
+        )
+        for move in self:
+            if move.manual_currency and not move.manual_currency_rate:
+                move.manual_currency_rate = (
+                    last_rate[self.company_id]
+                    if move.type_currency == "inverse_company_rate"
+                    else (1.0 / last_rate[self.company_id])
+                )
+    def _check_manual_currency_rate(self):
+        for rec in self:
+            if not rec.manual_currency:
+                continue
+            if float_is_zero(rec.manual_currency_rate, precision_digits=12):
+                raise UserError(_("The Manual Currency Rate must not be zero."))
+
+    def _post(self, soft=True):
+        self._check_manual_currency_rate()
+        return super()._post(soft=soft)
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
